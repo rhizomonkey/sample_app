b8b95889e669894684b74ca89adf8339a64ecc18
Jon Hoover <rhizomonkey@gmail.com>
2013-03-21 13:35:02 -0400

    Finish sign i

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,7 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it {should be_valid}
@@ -105,6 +106,11 @@ describe "with a password that's too short" do
       @user.save
       @user.reload.email.should == mixed_case_email.downcase
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
  
 end
